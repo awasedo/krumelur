b3c6080613daa8b84ea6a2b91e53251e67377cf2
Eskil Darpe <eskil@darpe.se>
2020-10-02 07:20:31 +0200

    Update Krumelur.py

--- Krumelur.py
+++ Krumelur.py
@@ -1,27 +1,22 @@
-import json
-
 from discord.ext.commands import Bot
-
 from utils.format import info
 import discord
 import json
 
-with open('data/config.json', 'r') as c:
+with open("utils/config.json", "r") as c:
     config = json.load(c)
-
 
 class Krumelur(Bot):
     def __init__(self):
-        super().__init__(command_prefix=config["bot_prefix"], owner_id=config["owner_id"])
+        super().__init__(command_prefix=config["krumelur_prefix"], owner_id=config["owner_id"])
 
         self.remove_command("help")
         self.embed_color = 0xff8800
         self.embed_error_color = 0xf81212
         self.embed_good_color = 0x12f82c
 
-        for cog in config["cogs"]:
-            self.load_extension(f"cogs.{cog}")
-            info(f"Loaded {cog}!")
+        for extension in config["extensions"]:
+            self.load_extension(f"cogs.{extension}")
+            info(f"Loaded {extension}!")
 
-
-Krumelur().run(config["bot_token"])
+Krumelur().run(config["krumelur_token"])
